bd524c6e8d27c3372a8fbbc1ba63ffd172294a29
Raj <rajmc222@gmail.com>
2022-07-22 18:22:22 +0530

    Updated User API to Update User Details.

--- authapi/views.py
+++ authapi/views.py
@@ -1,8 +1,10 @@
 """
 Views for User API.
 """
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.settings import api_settings
 from .serializers import UserSerializer, AuthTokenSerializer
 
@@ -12,7 +14,21 @@ class CreateUserView(CreateAPIView):
 
     serializer_class = UserSerializer
 
+
 class CreateTokenView(ObtainAuthToken):
     "Create a new auth Token for User."
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [
+        TokenAuthentication,
+    ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get_object(self):
+        return self.request.user
